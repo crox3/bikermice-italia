6bfb37231213de9b1d337ec1c6280adf01c86eaa
loris.croci <loris.croci@tinext.com>
2022-04-25 23:57:22 +0200

    First initial commit

---
+++ bikermice-italia-json/src/main/java/com/bikermice/json/JsonModule.java
@@ -0,0 +1,13 @@
+package com.bikermice.json;
+
+/**
+ * This class is optional and represents the configuration for the bikermice-italia-json module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/bikermice-italia-json</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * See https://documentation.magnolia-cms.com/display/DOCS/Module+configuration for information about module configuration.
+ */
+public class JsonModule {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
